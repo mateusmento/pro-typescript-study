95dd9cda393b99a3105c83c52a496576bb539110
Mateus Sarmento <indiementook@gmail.com>
2018-10-21 13:38:36 +0000

    Chapter 1 WIP - topic 18 completed

--- ch1/18-type-assertions.ts
+++ ch1/18-type-assertions.ts
@@ -1,4 +1,4 @@
-interface Rectangle
+export interface Rectangle
 {
 	x: number;
 	y: number;
@@ -13,8 +13,38 @@ interface Circle
 	radius: number;
 }
 
-let shape: Circle | Rectangle;
+let shape: Rectangle | Circle;
+
 shape = { x: 4, y: 3, radius: 2 };
+let radius: number = shape.radius;
 
 let circle: Circle = shape;
-let radius: number = shape.radius;
+// ERROR: Type Circle is not assignable to type Rectangle - width is missing in type Circle
+// let rect: Rectangle = shape;
+
+shape = { x: 4, y: 7, width: 10, height: 6};
+let width: number = shape.width;
+
+// ERROR: Type Rectangle is not assignable to type Circle - radius is missing in type Rectangle
+// circle: Circle = shape;
+
+
+// ERROR: Wrong conversion
+// circle = <Circle> shape;
+// let num: number = <number> '123';
+
+
+// Not an error but wrong code
+circle = <Circle> <any> shape;
+
+
+function getShape(type: string): Rectangle | Circle | undefined
+{
+	type = type.toLowerCase();
+	if (type === 'rectangle') return {x: 0, y:0, width: 10, height: 10};
+	else if (type === 'circle') return {x:0, y:0, width:10, height:10};
+}
+
+circle = <Circle>getShape('circle');
+circle = <Circle>getShape('rectangle');
+
